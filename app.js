b2b9b6977fe402c10a84884e6ae35045acca1cc5
andrescasa-dev <andrescasa.dev@gmail.com>
2022-02-16 19:07:21 -0500

    the asynchronous solution of load images was added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_ROOT = 'https://graph.instagram.com';
-const ACCESS_TOKEN = 'IGQVJYNVgzRjBTLWUzUHQ1OExPazFtTG9aNDI1b3VQUUZAybEh1UXRlYlBLTDBkblJzaFRZAVU0wTnBqWnNmZAEgwbE9Rd3Fkd0syYXc4aEd4TFJoak5CRno3dUtzR2dsUm5kWkpfRzRINWxlcVdWLVdObTRBYzU2QmxXX1Fv';
+const ACCESS_TOKEN = 'IGQVJYWXZAVb252ZAnBaTEVJcEhjejlFTGZASQUpLMTZAsMUYzT2JEbkZAvTlhybTV2Y0N5Rnk3b1lPR2JhLXg5TUlOTWNjTFc1bGFIOXB3WTdCdTFCakdGQ0JpaHBTa3YyM25RbW9hYVJPWGkxNHVjWGZAIS1NHemhFMXJGZATRr';
 const photos = document.getElementById('photos-container');
 console.log('DOM photos: ' + photos);
 
@@ -23,37 +23,9 @@ async function getUserIdImages(){
 
 async function getImageUrl(image_id){
   const response = await fetch(`${API_ROOT}/${image_id}?fields=media_url&access_token=${ACCESS_TOKEN}`);
-  const meidainfo = await response.json();
-  return meidainfo.media_url;
+  const mediaInfo = await response.json();
+  return mediaInfo.media_url;
 }
-
-// getUserIdImages()
-// .then(idsImages => {
-//   idsImages.forEach(id => {
-//     getImageUrl(id)
-//     .then(imageUrl => {
-//       const img = document.createElement('img');
-//       img.src = imageUrl;
-//       console.log(img);
-//       photos.appendChild(img);
-//   });
-// })});
-
-// async function displayPhotos(){
-//   const fragment = document.createDocumentFragment();
-//   const idsImages = await getUserIdImages();
-//   idsImages.forEach( 
-//     async function(id){
-//     const imageUrl = await getImageUrl(id);
-//     const img = document.createElement('img');
-//     img.src = imageUrl;
-//     console.log('se agrogo imagen al fragment')
-//     fragment.appendChild(img);
-//   });
-//   console.log('se agrego el fragment al DOM');
-//   photos.append(fragment);
-// }
-// displayPhotos();
 
 function appendImageToFragment(img_url, fragment){
   const img = document.createElement('img');
@@ -62,37 +34,37 @@ function appendImageToFragment(img_url, fragment){
   console.log('the image was added to the fragment');
 }
 
-const IDtoImgInAFragment = (id, fragment) =>{
-  return new Promise((resolve,reject)=>{
-    resolve(
-      getImageUrl(id)
-      .then(
-        img_url => appendImageToFragment(img_url, fragment)
-      )
-    )
-  })
-}
-
-
 async function loadImageFragment(ids, fragment){
-  //el prome está aqui, no encuentro una manera de hacerle await al foreach por lo que se vuelve asincrono y me retorna el valor.
+  console.log("starting adding images to the fragment")
   const promises = [];
   ids.forEach( 
     id => {
-      promises.push(IDtoImgInAFragment(id,fragment));
+      promises.push(
+        getImageUrl(id)
+        .then( 
+          imgUrl => appendImageToFragment(imgUrl, fragment)
+        )
+      );
     }
+  )
+  await Promise.all(promises)
+  .then(() =>{
+      console.log("the fragment was loaded")
+      photos.append(fragment)      
+    }   
   );
-  //ejecuta todas las promesas y luego si retorneme el fragmen
-  Promise.all(promises).then( values => console.log(values));
-  return fragment;
 }
 
 async function displayPhotos(){
   let fragment = document.createDocumentFragment();
   const idsImages = await getUserIdImages();
-  fragment = await loadImageFragment(idsImages, fragment);
-  photos.append(fragment);
-  console.log('the fragment was added to the DOM');
+  console.log('1');
+  loadImageFragment(idsImages, fragment)
+  .then(
+    value => {
+      console.log('finish');
+    }
+  );
 }
 
 displayPhotos();
